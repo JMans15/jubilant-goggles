420e74541af843772001285a47dd6cf59fb54dec
JMans15 <33575924+JMans15@users.noreply.github.com>
2019-03-24 13:34:25 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,35 +1,183 @@
 import pygame as pg
 import classes
-
-
+import ressources
+import functions as f
+import time
+import math
+import random
+#19h36 : Si Antoine & Thibaud ont un jour un enfant, il s'appelera Donatole
 pg.init()
 
-w = h = 600
+ressources.musiqueS.play(loops = -1)
+w = 800
+h = 600
 win = pg.display.set_mode((w,h))
-pg.display.set_caption("Le jeu pute à clic")
+pg.display.set_caption("La vague 126 vas vous surprendre !!! Je n'y aurai jamais cru")
+pg.display.set_icon(ressources.iconeI.convert_alpha())
 clock = pg.time.Clock()
-player = classes.Player(300,300)
-gameObject = [player]
-
-win.fill((0,255,255))
-pg.display.flip()
+gameManager = classes.GameManager()
 
 run = True
+gameRunning = True
 while run:
+    gameOverRun = True
 
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            run = False
+    player = classes.Player(w/2,h/2, w, h)
+    enemies = []
+    blyat = []
+    cooldown = 0
+    omg = classes.OMG()
+    circle = classes.Circle()
+    scared = classes.ScaredSmiley()
 
-    for go in gameObject:
-        go.update()
+    win.fill((177, 201, 239))
+    pg.display.flip()
+    wave = 9
+    waveFinished = False
+    counter = 0
+    num_enemy = 0
+    target_circle = []
+    target_scared = []
+    isWaveBoss = False
 
-    dirty_rect = []
+    while gameRunning:
 
-    for go in gameObject:
-        dirty_rect.extend(go.draw(win))
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                run = False
+                gameRunning = False
+                gameOverRun = False
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_1 and omg.wait == 0:
+                    omg.omgTime = 100
+                    omg.wait = omg.cooldown
+                if event.key == pg.K_2 and circle.wait == 0:
+                    circle.csize = 1
+                    target_circle = list(enemies)
+                    circle.wait = circle.cooldown
+                if event.key == pg.K_3 and scared.wait == 0:
+                    scared.scaredTime = 300
+                    target_scared = list(enemies)
+                    scared.wait = scared.cooldown
 
-    pg.display.update(dirty_rect)
-    clock.tick(30)
+        if pg.mouse.get_pressed()[0] and cooldown <= 0:
+            blyat.append(player.shoot())
+            cooldown = 1/player.fireRate
+
+        if waveFinished:
+            ressources.waveClearE.play()
+            gameManager.waveClear = True
+            gameManager.counter_wc = 0
+            wave += 1
+            if wave == 26:
+                wave = 27
+            if wave == 1001:
+                wave = 26
+            waveFinished = False
+            if wave % 10 == 0: isWaveBoss = True
+            #print("Wave finish", num_enemy, enemies)
+        else:
+            if num_enemy <= 0 and len(enemies) == 0:
+                if isWaveBoss:
+                    print(wave, num_enemy)
+                    f.generateBoss(wave,w,h,player,win,enemies, gameManager)
+                    isWaveBoss = False
+                else:
+                    #print(num_enemy, enemies)
+                    waveFinished = True
+                    counter = 0
+                    num_enemy = 2*round(math.sqrt(wave))
+                    f.generateEnemy(w,h,enemies,player, 0)
+
+            if num_enemy > 0 and counter % 20 == 0:
+                print(num_enemy)
+                f.generateEnemy(w,h,enemies,player, random.randint(0,1))
+                num_enemy -= 1
+
+        counter += 1
+        if cooldown >= 0: cooldown -= 1
+
+        enemies_blyat = []
+        for e in enemies:
+            try:
+                enemies_blyat.extend(e.bullets)
+            except:
+                pass
+
+        win.fill((177, 201, 239))
+
+        f.testColide(player, enemies, blyat, enemies_blyat, win)
+
+        if player.dead():
+            gameRunning = False
+
+        to_remove = []
+        for gameObjects in ([player],enemies,blyat):
+            for go in gameObjects:
+                go.update()
+                go.draw(win)
+                if not go.exist:
+                    to_remove.append(go)
+
+        for go in to_remove:
+            for gameObjects in (enemies,blyat):
+                try:
+                    gameObjects.remove(go)
+                except:
+                    try:
+                        target_scared.remove(go)
+                    except:
+                        try:
+                            target_circle.remove(go)
+                        except:
+                            pass
+
+
+
+        omg.effet(player, win)
+        circle.effet(target_circle, win,w,h)
+        scared.effet(target_scared, win)
+        if gameManager.isWarning:
+            gameManager.counter_w += 1
+            f.texts(f"Vague {wave} : SA TOURNE MAL",0,0,win,h//20,w,h, milieu = True)
+            if gameManager.counter_w >= 30:
+                gameManager.isWarning = False
+        elif gameManager.waveClear:
+            gameManager.counter_wc += 1
+            f.texts(f"Vague {wave}",h*1//3,h*2//3,win,h//20,w,h,milieu = True)
+            if gameManager.counter_wc >= 20:
+                gameManager.waveClear = False
+
+        f.texts(f"Vies: {player.hp}",w-100,10,win,30)
+        f.texts(f"Vague {wave}",10,10,win,50)
+        if omg.wait != 0:
+            f.texts('1 - OMG: '+str(omg.wait//30),10,h-30,win,20)
+        else:
+            f.texts('1 - OMG OK',10,h-30,win,20)
+        if circle.wait != 0:
+            f.texts(f'2 - Red Circle: {circle.wait//30}',(w//2)-100,h-30,win,20)
+        else:
+            f.texts('2 - Red Circle OK',(w//2)-100,h-30,win,20)
+        if scared.wait != 0:
+            f.texts(f'3 - Scared Smiley: {scared.wait//30}',w-200,h-30,win,20)
+        else:
+            f.texts('3 - Scared Smiley OK',w-200,h-30,win,20)
+        clock.tick(30)
+        pg.display.flip()
+
+
+    while gameOverRun:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                run = False
+                gameOverRun = False
+            if event.type == pg.KEYDOWN:
+                gameOverRun = False
+
+        f.texts("GAME OVER", (w//2)-100, (h//2)-20, win, 50)
+        f.texts("press ANY key to restart", (w//2) - 60, (h//2)+30, win, 20)
+        pg.display.flip()
+
+    gameRunning = True
 
 pg.quit()
--- ressources.py
+++ ressources.py
@@ -1,11 +1,47 @@
 import pygame.image as img
 import pygame.mixer as mix
+import pygame.display as disp
+import pygame as pg
+
 mix.init()
-img.init()
+disp.init()
+
+#musics
+def getSound(name):
+    return mix.music.load(f'Ressources\\{name}.mp3')
+
+def getEffect(name):
+    return mix.Sound(f'Ressources\\{name}.wav')
+
 #images
 def getImg(name):
-    return img.load("{}.png".format(name)).convert()
-r_arrowI = getImg('r_arrow')
-enemyI = getImg('enemy')
-p
-#musics
+    return img.load(f"Ressources\\{name}.png")
+scale = pg.transform.scale
+
+pouceI = scale(getImg('pouce-bleu'),(40,40))
+enemyI = scale(getImg('enemy'),(60,60))
+enemyShooterI = scale(getImg('enemyShooter'),(60,60))
+playerI = getImg('player')
+r_arrowI = scale(getImg('r_arrow'),(40,40))
+r_circleI = getImg('r_cercle')
+scared_smileyI = scale(getImg('smiley'), (40,40))
+bossI = pg.transform.scale(getImg('boss'),(160,160))
+iconeI = getImg("icone")
+
+shootE = getEffect("shoot")
+enemyDieE = getEffect("enemy_death")
+hitEnemyE = getEffect("hitEnemy")
+hitPlayerE = getEffect("hitPlayer")
+spawnBossE = getEffect("spawnBoss")
+waveClearE = getEffect("waveClear")
+
+shootE.set_volume(0.15)
+enemyDieE.set_volume(0.1)
+hitEnemyE.set_volume(0.15)
+hitPlayerE.set_volume(0.3)
+spawnBossE.set_volume(0.7)
+waveClearE.set_volume(0.5)
+
+
+getSound('musique')
+musiqueS = mix.music
